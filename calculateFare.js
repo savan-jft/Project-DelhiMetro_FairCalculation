9afc52af1a870e72c68a8f2100b1a598308ede13
savan-jft <savan.prajapati@jellyfishtechnologies.com>
2022-07-22 02:40:16 +0530

    logic added of fair calculation

--- calculateFare.js
+++ calculateFare.js
@@ -1,1 +1,45 @@
+var noOfStations = Math.abs(source-destination);
+var initialfair = 0;
 
+if(noOfStations>=0 || noOfStations<=4){
+    initialfair = 10;
+  } 
+  else if(noOfStations>=5 || noOfStations<=8){
+    initialfair = 20;
+  }
+  else if(noOfStations>=9 || noOfStations<=12){
+    initialfair = 30;
+  }
+  else if(noOfStations>=13 || noOfStations<=16){
+    initialfair = 40;
+  }
+  else if(noOfStations>=17 || noOfStations<=20){
+    initialfair = 50;
+  }
+  else if(noOfStations>=21 || noOfStations<=24){
+    initialfair = 60;
+  }
+  else{
+    initialfair = 70;
+  }
+
+//let initialfair = 20;
+let disc = 10;
+paymentMethod = 'A';
+function disc(fair){
+    if(noOfStations == 0){
+        return initialfair;
+    }
+    else
+    {
+        if(paymentMethod == 'A'){
+            return fair-(Math.floor((fair*disc)/100));
+          }
+        else{
+            return fair;
+          }
+    }
+}
+
+let final_fair = desc(initialfair);
+console.log(final_fair);
--- stationList.js
+++ stationList.js
@@ -1,7 +1,7 @@
-// Selecting Source and getting its value
 var stationList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+// Selecting Source and getting its value
 var selectSource = document.getElementById("sourceStation"), stationList;
-    //arr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 for(let i=0; i<stationList.length; i++){
     var option = document.createElement("OPTION"),
@@ -10,16 +10,17 @@ for(let i=0; i<stationList.length; i++){
     option.setAttribute("value", stationList[i]);
     selectSource.insertBefore(option,selectSource.lastChild);
 }
+
 function onSelectSource(){
     var source = document.getElementById('sourceStation').selectedIndex;
     console.log(source);
 }
+//console.log(source);
 
 
 
 // Selecting Destination and getting its value
 var selectDestination = document.getElementById("destStation"), stationList;
-    //arr = ['A','B','C','D','E','F','G','H','I'];
 
 for(let i=0; i<stationList.length; i++){
     var option = document.createElement("OPTION"),
@@ -35,13 +36,24 @@ function onSelectDest(){
 }
 
 
+
 //getting payment method
+
 function paymentMethod(){
     var payMethod = document.getElementById('payMethod').selectedIndex;
     var method = document.getElementsByTagName("option")[payMethod].value;
     console.log(method);
-    console.log(typeof method);
 }
+
+//console.log(method);
 
 
 // calculating fare between stations
+
+var a = 10;
+var b = 1;
+var value = Math.abs(b-a);
+//console.log(value)
+
+
+
