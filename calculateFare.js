a8a12f11aa2a49a373759f55e2445a84c39da7a5
savan-jft <savan.prajapati@jellyfishtechnologies.com>
2022-07-24 18:22:38 +0530

    final commit

--- calculateFare.js
+++ calculateFare.js
@@ -2,76 +2,42 @@ function dropDown(){
     var stationList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     for(let i=0;i<=stationList.length-1;i++)
     {
-    document.getElementById('sourceStation').innerHTML += `<option value=${i+1}>${stationList[i]}</option>`;
+    document.getElementById('sourceStation').innerHTML += `<option value=${i+1}>Station-${stationList[i]}</option>`;
  
-    document.getElementById('destStation').innerHTML += `<option value=${i+1}>${stationList[i]}</option>`;
+    document.getElementById('destStation').innerHTML += `<option value=${i+1}>Station-${stationList[i]}</option>`;
     }
 }
 dropDown();
 
 
-
-function calculateFair(){
+function calculateFare(){
     let initialDest = document.getElementById('sourceStation').value;
     // console.log(initialDest);
     let finalDest = document.getElementById('destStation').value;
     // console.log(finalDest);
 
-    let noOfStations =Math.abs(initialDest - finalDest);
+    let noOfStations = Math.abs(initialDest - finalDest);
 
-    var fair = 0;
+    var fare = 0;
 
-    if(noOfStations>=0 && noOfStations<=4){
-        fair = 10;
-    } 
-    else if(noOfStations>=5 && noOfStations<=8){
-        fair = 20;
+    if(noOfStations < 1 ){
+        fare = 10;
     }
-    else if(noOfStations>=9 && noOfStations<=12){
-        fair = 30;
-    }
-    else if(noOfStations>=13 && noOfStations<=16){
-        fair = 40;
-    }
-    else if(noOfStations>=17 && noOfStations<=20){
-        fair = 50;
-    }
-    else if(noOfStations>=21 && noOfStations<=24){
-        fair = 60;
+
+    if(noOfStations %4 == 0){
+        fare = (Math.floor(noOfStations/4)*10);
     }
     else{
-        fair = 70;
+        fare = (Math.floor(noOfStations/4 + 1)*10);
     }
 
-
-
-    let payment1 = document.getElementById('card').value;
-    let payment2 = document.getElementById('token').value;
-
-    console.log(payment1);
-    console.log(payment2);
-    console.log(typeof payment1);
-    console.log(typeof payment2);
-
-    let discount = 10;
-    
-    function disc(fair){
-        if(noOfStations <= 4){
-            return fair;
-        }
-        else
-        {
-            if(payment1 == 'token'){
-                return fair-(Math.floor((fair*discount)/100));
-            }
-            else{
-                return fair;
-            }
-        }
+    if(noOfStations > 24){
+        fare = 60;
     }
 
+    if(document.getElementById('payMethod').value == 'card' && noOfStations >= 5){
+        fare = fare - fare*0.1;
+    }
 
-    let final_fair = disc(fair);
-    console.log(final_fair);
-    document.getElementById('fare').value = final_fair;
+    document.getElementById('fare').value = fare;
 }
